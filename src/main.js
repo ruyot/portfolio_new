1e7b45e1d4ede2a190f94537085c30027a044c65
ruyot <tabeeb700@gmail.com>
2026-02-03 00:18:59 -0500

    modularization for css styling

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import './style.css'
+import './styles/main.css'
 import './zoomControl.js'
 import { typeCommand, scrambleText, revealLineByLine } from './animations.js'
 import { initViewToggle } from './viewToggle.js'
